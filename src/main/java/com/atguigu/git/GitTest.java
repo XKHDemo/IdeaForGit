0d5c06a48e2bd0417a01192ef6bf92173bada4eb
xf <6199@qq.com>
2022-01-26 02:02:11 +0800

    first Commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author fengxu  Email:619928024@qq.com
+ * @Description
+ * @create 2022012022/1/26}1:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
